1473daf34bd3b3d06091106550927256f2f98e0d
carvalhovinni <carlosvivi963@gmail.com>
2023-06-13 11:38:04 -0300

    metodo main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
          // Aula introdutória de Orientação à Objetos
+             // criar objetos
+        Pessoa adao = new Pessoa();
 
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,18 @@
 public class Pessoa {
     // Atributo
-    String nome;
+
+    String sobrenome;
+
+    // métodos de classes
+
+    public void falar() { System.out.println("falei"); }
+
+    public void(String volume) { System.out.println("falei " + volume); }
 }
+
+   public void falar (String volume;
+       String tom){
+    return "falei = volume";
+
+  }
+        }
